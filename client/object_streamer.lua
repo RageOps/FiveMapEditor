aa00f11850dcf3a0c91094a3d99fabd24f00eb15
RageOps <Dschneid098@gmail.com>
2022-06-01 13:31:47 -0400

    Update object_streamer.lua

--- client/object_streamer.lua
+++ client/object_streamer.lua
@@ -1,5 +1,6 @@
 local objectSets
 local removeSets
+local placedObjects
 
 RegisterNetEvent('MapEdit:SendObjectSets', function(sets)
     objectSets = sets
@@ -9,12 +10,16 @@ RegisterNetEvent('MapEdit:SendRemoveSets', function(sets)
     removeSets = sets
 end)
 
+RegisterNetEvent('MapEdit:SendPlacedObjects', function(sets)
+    placedObjects = sets
+end)
+
 -- object streamer
 local function isNearObject(p1, obj)
 	local diff = obj.pos - p1
 	local dist = (diff.x * diff.x) + (diff.y * diff.y)
 
-	return (dist < (400 * 400))
+	return (dist < (500 * 500))
 end
 
 Citizen.CreateThread(function()
@@ -56,9 +61,9 @@ end)
 Citizen.CreateThread(function()
     while true do
         Wait(500)
-        if removeSets then
+        if removeSets and placedObjects then
+            local pos = GetEntityCoords(PlayerPedId())
             for k, v in pairs(removeSets) do
-                local pos = GetEntityCoords(PlayerPedId())
                 v.pos = vector3(v.pos.x, v.pos.y, v.pos.z)
                 if #(pos - v.pos) < 150 then
                     local ent = GetClosestObjectOfType(v.pos, 1.5, v.model, false, false, false)
@@ -67,9 +72,35 @@ Citizen.CreateThread(function()
                     SetEntityAsNoLongerNeeded(ent)
                 end
             end
+
+            for k, v in pairs(placedObjects) do
+                local pedPos = GetEntityCoords(PlayerPedId())
+                local model = v[1]
+                local coords = v[2]
+                local h = v[3]
+                local pos = vector3(coords.x, coords.y, coords.z)
+                local heading = vector3(h.x, h.y, h.z)
+                if #(pos - pedPos) < 80 and not v.object then
+                    local obj = CreateObjectNoOffset(model, pos, true, true)
+                    if obj then
+                        SetEntityHeading(obj, heading)
+                        FreezeEntityPosition(obj, true)
+                        SetObjectForceVehiclesToAvoid(obj, true)
+
+                        v.object = obj
+                    end
+                elseif #(pos - pedPos) > 80 and v.object then
+                    local ent = GetClosestObjectOfType(pos, 1.5, model, false, false, false)
+                    SetEntityAsMissionEntity(ent, 1, 1)
+                    DeleteObject(ent)
+                    SetEntityAsNoLongerNeeded(ent)
+                    v.object = nil
+                end
+            end
         else
             Wait(1500)
             TriggerServerEvent('MapEdit:GetRemoveSets')
+            TriggerServerEvent('MapEdit:GetPlacedObjects')
         end
     end
 end)
